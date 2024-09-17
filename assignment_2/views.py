22fd7335c59d5c67813387c05ecdc6a463d0f712
rakib444007 <rh444007@gmail.com>
2024-09-17 13:19:29 +0600

    first one

---
+++ assignment_2/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("This is Home Page......")
+
+
+def contact(request):
+    return HttpResponse("This is Contact Pages----------------")
